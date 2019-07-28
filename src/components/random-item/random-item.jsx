99e829caa20b7a6260fd4af227d45cb8f408449c
Roman Makretki <r.makretski@gmail.com>
2019-07-28 20:09:40 +0300

    featL implement spinner in random-item component

--- src/components/random-item/random-item.jsx
+++ src/components/random-item/random-item.jsx
@@ -1,32 +1,68 @@
 import React, { Component } from 'react';
+import Spinner from '../spinner';
+import SwapiService from '../../services/swapi-service';
 import './random-item.css';
 
+
+const ItemView = ({ planet }) => {
+  const { imgUrl, name, population, rotationPeriod, diameter } = planet;
+  return (
+    <React.Fragment>
+      <img
+        className="random-item__item-image"
+        src={imgUrl}
+        alt="Planet name"
+      />
+      <div>
+        <h2>{name}</h2>
+        <ul className="random-item__description list-group-flush">
+          <li className="random-item__description">
+            <span className="term">Population</span>
+            <span>{population}</span>
+          </li>
+          <li className="random-item__description">
+            <span className="term">Rotation Period</span>
+            <span>{rotationPeriod}</span>
+          </li>
+          <li className="random-item__description">
+            <span className="random-item__description-term-text">Diameter</span>
+            <span>{diameter}</span>
+          </li>
+        </ul>
+      </div>
+    </React.Fragment>
+  );
+};
+
 export default class RandomItem extends Component {
+
+  constructor() {
+    super();
+    this.setRandomPlanet();
+  }
+
+  state = {
+    planet: {},
+    loading: true,
+  };
+
+  swapiService = new SwapiService();
+
+  onPlanetLoad = (planet) => {
+    this.setState({ planet: { ...planet }, loading: false })
+  };
+
+  setRandomPlanet = () => {
+    this.swapiService.getRandomPlanet()
+      .then(this.onPlanetLoad);
+  }
+
   render() {
+    const { loading, planet } = this.state;
+
     return (
       <div className="random-item jumbotron rounded">
-        <img
-          className="random-item__item-image"
-          src="https://starwars-visualguide.com/assets/img/planets/5.jpg"
-          alt="Planet name"
-        />
-        <div>
-          <h2>Planet Name</h2>
-          <ul className="random-item__description list-group-flush">
-            <li className="random-item__description">
-              <span className="term">Population</span>
-              <span>123124</span>
-            </li>
-            <li className="random-item__description">
-              <span className="term">Rotation Period</span>
-              <span>43</span>
-            </li>
-            <li className="random-item__description">
-              <span className="random-item__description-term-text">Diameter</span>
-              <span>100</span>
-            </li>
-          </ul>
-        </div>
+        {loading ? <Spinner /> : <ItemView planet={planet} />}
       </div>
     );
   }
