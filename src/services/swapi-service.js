9d3c61770c52e3cc8761641f184f397cc27cdf19
Roman Makretki <r.makretski@gmail.com>
2019-07-28 19:11:23 +0300

    feat: implement getRandomPLanet method for swapi-service

--- src/services/swapi-service.js
+++ src/services/swapi-service.js
@@ -1,5 +1,6 @@
 export default class SwapiBD {
     #url = 'https://swapi.co/api/';
+    #imgUrl = 'https://starwars-visualguide.com/assets/img/';
 
     getData(url) {
         return fetch(url)
@@ -12,33 +13,61 @@ export default class SwapiBD {
             .catch(window.console.log.bind(window.console));
     }
 
+    getImageUrl(type, id) {
+        return `${this.#imgUrl}${type}/${id}.jpg`;
+    }
+
     getAllPersons() {
         const url = `${this.#url}people`;
-        return this.getData(url);  
+        return this.getData(url);
     }
 
     getPerson(id) {
         const url = `${this.#url}people/${id}`;
-        return this.getData(url); 
+        return this.getData(url);
     }
 
     getAllStarships() {
         const url = `${this.#url}starships`;
-        return this.getData(url);  
+        return this.getData(url);
     }
 
     getStarship(id) {
         const url = `${this.#url}starships/${id}`;
-        return this.getData(url); 
+        return this.getData(url);
     }
 
     getAllPlanets() {
         const url = `${this.#url}planets`;
-        return this.getData(url);  
+        return this.getData(url).then(planets => planets.map(this.transformPlanet));
     }
 
     getPlanet(id) {
         const url = `${this.#url}planets/${id}`;
-        return this.getData(url); 
+        return this.getData(url)
+            .then(this.transformPlanet);
+    }
+
+    getRandomPlanet() {
+        const randomID = Math.floor(Math.random() * 19) + 1;
+        return this.getPlanet(randomID);
+    }
+
+    getIdFromUrl(url) {
+        const idRegexp = /([0-9]+)/;
+        return url.match(idRegexp)[1];
+    }
+
+    transformPlanet = (planet) => {
+        const id = this.getIdFromUrl(planet.url);
+        const imgUrl = this.getImageUrl('planets', id);
+
+        return {
+          imgUrl,
+          name: planet.name,
+          population: planet.population,
+          rotationPeriod: planet.rotationPeriod,
+          diameter: planet.diameter,
+        }
     }
 }
