358264bfecfe02b5f41c325001fc88941439dfd7
Roman Makretki <r.makretski@gmail.com>
2019-07-29 02:02:23 +0300

    feat: implement item-list component

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,25 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from '../header';
 import RandomItem from '../random-item';
 import ItemList from '../item-list';
 import PersonDetails from '../person-details';
 import './app.css';
 
-const App = () => (
-  <div className="app-wrapper">
-    <Header />
-    <RandomItem />
-    <main className="row mb2">
-      <div className="col-md-4">
-        <ItemList />
-      </div>
-      <div className="col-md-8">
-        <div>
-          <PersonDetails />
-        </div>
-      </div>
-    </main>
-  </div>
-);
+export default class App extends Component {
 
-export default App;
+  onItemSwap = (item) => {
+    window.console.dir(item);
+  }
+
+  render() {
+    return (
+      <div className="app-wrapper">
+        <Header />
+        <RandomItem />
+        <main className="row mb2">
+          <div className="col-md-4">
+            <ItemList onItemClick={this.onItemSwap} />
+          </div>
+          <div className="col-md-8">
+            <div>
+              <PersonDetails />
+            </div>
+          </div>
+        </main>
+      </div>
+    )
+  }
+}
--- src/components/item-list/item-list.jsx
+++ src/components/item-list/item-list.jsx
@@ -1,19 +1,65 @@
 import React, { Component } from 'react';
+import SwapiService from '../../services/swapi-service';
+import Spinner from '../spinner';
+import ErrorIndicator from '../error-indicator'
 import './item-list.css';
 
 export default class ItemList extends Component {
+
+  state = {
+    persons: [],
+    isLoad: true,
+    isError: false,
+  }
+
+  swapiService = new SwapiService();
+
+  onPersonsLoad = (persons) => {
+    this.setState({
+      persons,
+      isLoad: false
+    });
+  }
+
+  onError = () => {
+    this.setState({
+      isError: true,
+      isLoad: false,
+    });
+  }
+
+  componentDidMount() {
+    this.swapiService.getAllPersons()
+      .then(this.onPersonsLoad)
+      .catch(this.onError);
+  }
+
+  getItemsElements() {
+    const { persons } = this.state;
+    const { onItemClick } = this.props;
+    return persons.map(person => (
+      <li
+        className="list-group-item item-list__item"
+        key={person.id}
+        onClick={
+          () => onItemClick(person.id)
+        }
+      >
+        {person.name}
+      </li>
+    ));
+  }
+
   render() {
+    const { isLoad, isError } = this.state;
+    const error = isError ? <ErrorIndicator /> : null;
+    const spinner = isLoad ? <Spinner /> : null;
+    const listItems = !isLoad && !isError ? this.getItemsElements() : null;
     return (
       <ul className="list-group item-list">
-        <li className="list-group-item item-list__item">
-          Luke Skywalker
-        </li>
-        <li className="list-group-item item-list__item">
-          Darth Vader
-        </li>
-        <li className="list-group-item item-list__item">
-          R2-D2
-        </li>
+        {error}
+        {spinner}
+        {listItems}
       </ul>
     );
   }
