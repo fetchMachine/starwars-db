0b7d187e097a90153fa718c0d7b55c6ec1180062
Roman Makretki <r.makretski@gmail.com>
2019-08-03 22:27:20 +0300

    feat: implement person-page component

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,35 +1,18 @@
 import React, { Component } from 'react';
 import Header from '../header';
 import RandomItem from '../random-item';
-import ItemList from '../item-list';
-import PersonDetails from '../person-details';
+import PersonPage from '../person-page';
 import './app.css';
 
 export default class App extends Component {
 
-  state = {
-    currItemID: 1,
-  }
-
-  onItemSwap = (itemID) => {
-    this.setState({ currItemID: itemID });
-  }
-
   render() {
-    const { currItemID } = this.state;
     return (
       <div className="app-wrapper">
         <Header />
         <RandomItem />
         <main className="row mb2">
-          <div className="col-md-4">
-            <ItemList onItemClick={this.onItemSwap} />
-          </div>
-          <div className="col-md-8">
-            <div>
-              <PersonDetails personID={currItemID} isLoad={true} />
-            </div>
-          </div>
+          <PersonPage />
         </main>
       </div>
     )
