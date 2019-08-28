57fefbbb5a8c9ab715f4ecb1c56cfcd1a5c4667c
Roman Makretki <r.makretski@gmail.com>
2019-08-28 20:22:07 +0300

    ref: remove withNetworkRequest

--- src/components/hoc-helpers/index.js
+++ src/components/hoc-helpers/index.js
@@ -1,9 +1,7 @@
-import withNetwortkRequest from './hoc-network-requests';
 import withSwapiService from './with-swapi-service';
 import WithNetwork from './hoc-network';
 
 export {
-  withNetwortkRequest,
   withSwapiService,
   WithNetwork,
 }
