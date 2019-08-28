8f54ad416c2b84e9f26d47008729404b244352a6
Roman Makretki <r.makretski@gmail.com>
2019-08-28 20:28:51 +0300

    ref: implement prop types for RowCol2

--- src/components/row-col2/row-col2.jsx
+++ src/components/row-col2/row-col2.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './row-col2.css';
 
 const RowCol2 = ({ left, right }) => (
@@ -11,5 +12,10 @@ const RowCol2 = ({ left, right }) => (
     </div>
   </div>
 );
+
+RowCol2.propTypes = {
+  left: PropTypes.node,
+  right: PropTypes.node,
+};
 
 export default RowCol2;
