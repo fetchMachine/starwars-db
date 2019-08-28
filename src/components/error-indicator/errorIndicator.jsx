22ff25c51b243dc54a04b272ada0dad5062c2421
Roman Makretki <r.makretski@gmail.com>
2019-08-28 20:17:04 +0300

    feat: add img to error-indicator

--- src/components/error-indicator/errorIndicator.jsx
+++ src/components/error-indicator/errorIndicator.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import ErrorImg from './death-star.png';
 import './errorIndicator.css';
 
 const ErrorIndicator = () => (
   <div className="error-indicator">
-    <span>Something go wrong. We already sent R2-D2 to fix this...</span>
+    <img src={ErrorImg} alt="error" />
+    <p>Something go wrong...</p>
+    <p>But We already sent R2-D2 to fix this...</p>
   </div>
 );
 
